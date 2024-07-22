6d70f19e3cfe527af287f1c348ce545e79b2689b
tibarekb <berassut@gmail.com>
2024-07-22 09:05:20 +0300

    remover css

--- mohan_ERP/settings.py
+++ mohan_ERP/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'orders',
        'USER': 'postgres',
-       'PASSWORD': 'postgres',
+       'PASSWORD': 'mohanPLC',
        'HOST': '127.0.0.1', 
         'PORT': '5432',
    }
