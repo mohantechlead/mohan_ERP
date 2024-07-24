7a26615d20cb2fa36444353062213b7c3beee134
shecode <110271708+tibarekb@users.noreply.github.com>
2024-07-24 14:37:19 +0300

    Update 0004_create_items_list.py

--- FGRN/migrations/0004_create_items_list.py
+++ FGRN/migrations/0004_create_items_list.py
@@ -1,7 +1,19 @@
-migrations.CreateModel(
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('FGRN', '0003_fgrn_item_per_unit_kg'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
             name='finished_goods',
             fields=[
                 ('item_name', models.TextField(primary_key=True, serialize=False)),
                 ('quantity', models.FloatField()),
             ],
         ),
+    ]
+
