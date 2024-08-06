f2c00d65e0a24cc1045ad6a30e6115e0cdf01bf0
shecode <110271708+tibarekb@users.noreply.github.com>
2024-08-06 10:03:28 +0300

    Update models.py

--- MR/models.py
+++ MR/models.py
@@ -19,6 +19,9 @@ class MR_item(models.Model):
     no_of_unit = models.FloatField(blank=True, null=True)
     per_unit_kg = models.FloatField(blank=True, null=True)
 
+    def __int__(self):
+        return self.MR_no
+
 class inventory(models.Model):
     item_name = models.TextField(blank=True)
     no_of_unit = models.FloatField(blank=True, null=True)
