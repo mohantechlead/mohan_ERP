4d4a0b67b9b258de63076c6a06182032f3c56799
tibarekb <berassut@gmail.com>
2024-08-12 17:26:20 +0300

    Inventory calculation Corrected

--- MR/admin.py
+++ MR/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import MR,MR_item, inventory
+from .models import *
 
 admin.site.register(MR)
 admin.site.register(MR_item)
 admin.site.register(inventory)
+admin.site.register(inventory_MR_items)
