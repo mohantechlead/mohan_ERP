8523fe5d93d7022878709387e827ea45c1b6804d
tibarekb <berassut@gmail.com>
2024-08-22 14:26:32 +0300

    FGRN items

--- MR/urls.py
+++ MR/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('display_MR_items',views.display_MR_items, name= 'display_MR_items'),
     path('export_mr',views.export_mr, name= 'export_mr'),
     path('stock_card',views.stock_card, name= 'stock_card'),
+    path('edit_mr/<int:MR_no>',views.edit_parent_and_children, name="edit_mr"),
     path('export_mr_pdf', views.export_mr_pdf, name='export_mr_pdf'),
 ]
