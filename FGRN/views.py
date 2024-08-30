4674e4d0273efa41d1303da650226f8dc46e0eec
tibarekb <berassut@gmail.com>
2024-08-30 17:09:55 +0300

    FGRN started

--- FGRN/views.py
+++ FGRN/views.py
@@ -14,107 +14,157 @@ from MR.models import *
 from .decorators import allowed_users
 from DN.models import delivery_items
 
-# Create your views here.
-# @login_required(login_url='login_user')
-# @allowed_users(allowed_roles=['admin'])
+
+# @login_required(login_url="login_user")
+# def create_fgrn(request):
+#     if request.method == 'POST':
+#         form = FGRNForm(request.POST)
+        
+#         if form.errors:
+#             print(form.errors)
+
+#         if form.is_valid():
+#             print(form.data,"val")
+
+#             form.save()
+#             return redirect('create_MR')
+#         else:
+#             print(form.data,"nval")
+#             errors = dict(form.errors.items())
+#             print(errors,"errors")
+#             return JsonResponse({'form_errors': errors}, status=400)
+#     form = FGRNForm()
+#     formset = formset_factory(FGRNItemForm, extra= 1)
+#     formset = formset(prefix="items")
+#     # print(formset)
+#     items = items_list.objects.all()
+#     context = {
+#             'my_item':items,
+#             'form': form, 
+#             'formset': formset
+#     }
+#     return render(request,'create_fgrn.html',context)
+
+# @login_required(login_url="login_user")
+# def create_fgrn_items(request):
+#     if request.method == 'POST':
+#         formset = formset_factory(FGRNItemForm, extra=1 , min_num= 1)
+#         formset = formset(request.POST or None, prefix="items")
+
+#         if formset.errors:
+#             print(formset.errors)
+
+#         non_empty_forms = [form for form in formset if form.cleaned_data.get('item_name')]
+#         pr_no = request.POST.get('FGRN_no')
+#         print(pr_no,"pr")
+#         if non_empty_forms:
+#             if formset.is_valid():
+#                 FGRN_instance = FGRN.objects.get(FGRN_no = pr_no)
+#                 final_quantity = 0.0
+#                 total_bag = 0.0
+#                 total_crt  = 0.0
+#                 total_pkg = 0.0
+#                 for form in non_empty_forms:
+#                     form.instance.FGRN_no = FGRN_instance
+                   
+#                     quantity = form.cleaned_data['quantity']
+#                     unit_type = form.cleaned_data['unit_type']
+#                     no_of_unit = form.cleaned_data['no_of_unit']
+#                     description = form.cleaned_data['description']
+
+#                     finished_item = finished_goods.objects.get(item_name = description)
+#                     if unit_type == 'Bag':
+#                         total_bag += no_of_unit
+#                     elif unit_type == 'Crt':
+#                         total_crt += no_of_unit
+#                     elif unit_type == 'Pkg':
+#                         total_pkg += no_of_unit
+#                     final_quantity += quantity
+#                     finished_item.quantity += quantity
+#                     finished_item.no_of_unit += no_of_unit
+#                     finished_item.save()
+#                     form.save()
+         
+#                     FGRN_instance.total_quantity = final_quantity
+#                     FGRN_instance.save()
+                    
+                    
+#             else:
+#                 print(formset.data,"nval")
+#                 errors = dict(formset.errors.items())
+#                 return JsonResponse({'form_errors': errors}, status=400)
+        
+#             pr_form = FGRNForm(prefix="orders")
+#             formset = formset_factory(FGRNItemForm, extra=1)
+#             formset = formset(prefix="items")
+
+#             context = {
+#                 'pr_form': pr_form,
+#                 'formset': formset,
+#                 # 'message':success_message,
+#             }
+#             return render(request, 'create_fgrn.html', context)
+#     else:
+       
+#         formset = formset_factory(FGRNItemForm, extra=1)
+#         formset = formset(prefix="items")
+
+#     context = {
+#         'formset': formset,
+#     }
+#     return render(request, 'create_fgrn.html', context)
+
 @login_required(login_url="login_user")
 def create_fgrn(request):
+    FGRNFormSet = formset_factory(FGRNItemForm, extra=1)
+
     if request.method == 'POST':
         form = FGRNForm(request.POST)
-        
-        if form.errors:
-            print(form.errors)
+        formset = FGRNFormSet(request.POST, prefix="items")
 
-        if form.is_valid():
-            print(form.data,"val")
+        if form.is_valid() and formset.is_valid():
+            # Save the MR form
+            FGRN_instance = form.save()
 
-            form.save()
-            return redirect('create_MR')
+            # Process the formset
+            for form_item in formset:
+                if form_item.cleaned_data.get('DELETE'):
+                    # If delete checkbox is checked, delete the item
+                    if form_item.instance.pk:
+                        form_item.instance.delete()
+                else:
+                    item_name = form_item.cleaned_data.get('item_name')
+                    quantity = form_item.cleaned_data.get('quantity')
+                    no_of_unit = form_item.cleaned_data.get('no_of_unit')
+
+                    if item_name:
+                        try:
+                            inventory_item = inventory.objects.get(item_name=item_name)
+                            inventory_item.quantity -= quantity
+                            inventory_item.no_of_unit -= no_of_unit
+                            inventory_item.save()
+                        except inventory.DoesNotExist:
+                            inventory_item = inventory(item_name=item_name, quantity=-quantity)
+                            inventory_item.save()
+
+                        # Save each MRItem form with the corresponding MR instance
+                        form_item.instance.FGRN_no = FGRN_instance
+                        form_item.save()
+
+            return redirect('create_FGRN')
+
         else:
-            print(form.data,"nval")
-            errors = dict(form.errors.items())
-            print(errors,"errors")
+            # Return the form errors if form or formset is invalid
+            form_errors = dict(form.errors.items())
+            formset_errors = {f"formset_{i}": dict(form_item.errors) for i, form_item in enumerate(formset) if form_item.errors}
+            errors = {**form_errors, **formset_errors}
             return JsonResponse({'form_errors': errors}, status=400)
-    form = FGRNForm()
-    formset = formset_factory(FGRNItemForm, extra= 1)
-    formset = formset(prefix="items")
-    # print(formset)
-    items = items_list.objects.all()
-    context = {
-            'my_item':items,
-            'form': form, 
-            'formset': formset
-    }
-    return render(request,'create_fgrn.html',context)
 
-@login_required(login_url="login_user")
-def create_fgrn_items(request):
-    if request.method == 'POST':
-        formset = formset_factory(FGRNItemForm, extra=1 , min_num= 1)
-        formset = formset(request.POST or None, prefix="items")
-
-        if formset.errors:
-            print(formset.errors)
-
-        non_empty_forms = [form for form in formset if form.cleaned_data.get('item_name')]
-        pr_no = request.POST.get('FGRN_no')
-        print(pr_no,"pr")
-        if non_empty_forms:
-            if formset.is_valid():
-                FGRN_instance = FGRN.objects.get(FGRN_no = pr_no)
-                final_quantity = 0.0
-                total_bag = 0.0
-                total_crt  = 0.0
-                total_pkg = 0.0
-                for form in non_empty_forms:
-                    form.instance.FGRN_no = FGRN_instance
-                   
-                    quantity = form.cleaned_data['quantity']
-                    unit_type = form.cleaned_data['unit_type']
-                    no_of_unit = form.cleaned_data['no_of_unit']
-                    description = form.cleaned_data['description']
-
-                    finished_item = finished_goods.objects.get(item_name = description)
-                    if unit_type == 'Bag':
-                        total_bag += no_of_unit
-                    elif unit_type == 'Crt':
-                        total_crt += no_of_unit
-                    elif unit_type == 'Pkg':
-                        total_pkg += no_of_unit
-                    final_quantity += quantity
-                    finished_item.quantity += quantity
-                    finished_item.no_of_unit += no_of_unit
-                    finished_item.save()
-                    form.save()
-         
-                    FGRN_instance.total_quantity = final_quantity
-                    FGRN_instance.save()
-                    
-                    
-            else:
-                print(formset.data,"nval")
-                errors = dict(formset.errors.items())
-                return JsonResponse({'form_errors': errors}, status=400)
-        
-            pr_form = FGRNForm(prefix="orders")
-            formset = formset_factory(FGRNItemForm, extra=1)
-            formset = formset(prefix="items")
-
-            context = {
-                'pr_form': pr_form,
-                'formset': formset,
-                # 'message':success_message,
-            }
-            return render(request, 'create_fgrn.html', context)
     else:
-       
-        formset = formset_factory(FGRNItemForm, extra=1)
-        formset = formset(prefix="items")
+        form = FGRNForm()
+        formset = FGRNFormSet(prefix="items")
 
-    context = {
-        'formset': formset,
-    }
-    return render(request, 'create_fgrn.html', context)
+    return render(request, 'create_fgrn.html', {'form': form, 'formset': formset})
 
 @login_required(login_url="login_user")
 def display_FGRN(request):
