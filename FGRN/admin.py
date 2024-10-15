889b06704278dd372af65551c7b896f5e7c183a9
tibarekb <berassut@gmail.com>
2024-10-15 08:55:01 +0300

    database configuration

--- FGRN/admin.py
+++ FGRN/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import FGRN,FGRN_item, finished_goods,items_list, FGRNopening_balance
+from .models import FGRN,FGRN_item, finished_goods,items_list, FGRNopening_balance, inventory_FGRN_items
 
 admin.site.register(FGRN)
 admin.site.register(FGRN_item)
 admin.site.register(finished_goods)
 admin.site.register(items_list)
 admin.site.register(FGRNopening_balance)
+admin.site.register(inventory_FGRN_items)
