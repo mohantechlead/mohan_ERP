bcd2e49d3b78fefc457390c6bb393daee4e1151b
shecode <110271708+tibarekb@users.noreply.github.com>
2024-11-14 11:05:01 +0300

    Update views.py

--- GRN/views.py
+++ GRN/views.py
@@ -603,7 +603,6 @@ def create_pr(request):
             #instance.PR_before_vat = 0.00
             #instance.PR_total_price = 0.00
             instance.excise_tax = excise
-            instance.vat = instance.total_price - instance.before_vat
 
             instance.save()
             
