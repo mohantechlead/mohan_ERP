c396303b7e3d611ca0b966abeda1e5c4b92a736f
shecode <110271708+tibarekb@users.noreply.github.com>
2024-11-27 09:05:58 +0300

    Update forms.py

--- FGRN/forms.py
+++ FGRN/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import *
-from DN.models import inventory_order_items
 
 
 class FGRNForm(forms.ModelForm):
@@ -27,25 +26,10 @@ class FGRNItemForm(forms.ModelForm):
         queryset=items_list.objects.all(),
         widget=forms.Select(attrs={'class': 'form-control', 'id':'item_name'}),
     )
-    # description = forms.ModelChoiceField(
-    #     queryset=finished_goods.objects.all(),
-    #     widget=forms.Select(attrs={'class': 'form-control', 'id':'description'}),)
-
-    description = forms.ChoiceField(
-    choices=[
-        (item.item_name, item.item_name)  # Only include item_name without the model name prefix
-        for item in sorted(
-            chain(inventory_order_items.objects.all(), finished_goods.objects.all()),
-            key=operator.attrgetter('item_name')
-        )
-    ],
-    widget=forms.Select(attrs={
-        'class': 'form-control select2',
-        'data-minimum-input-length': '0',  # Start filtering from the first character
-        'data-placeholder': 'Select or type an item',
-        'id': 'description'
-    }),
-)
+    
+    description = forms.ModelChoiceField(
+        queryset=finished_goods.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control', 'id':'description'}),)
     
     no_of_unit = forms.FloatField(
         required = False,
