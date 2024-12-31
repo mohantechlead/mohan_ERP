84c930ec0163295eef4c9239751be62d9d939513
shecode <110271708+tibarekb@users.noreply.github.com>
2024-12-31 16:26:13 +0300

    Update 0028_alter_customer_phone_number.py

--- DN/migrations/0028_alter_customer_phone_number.py
+++ DN/migrations/0028_alter_customer_phone_number.py
@@ -1,1 +1,17 @@
-("phone_number", models.CharField(max_length=15)),
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('DN', '0026_inventory_order_items'),
+    ]
+
+   operations = [
+        migrations.AlterField(
+            model_name="delivery",
+            name="phone_number",
+            field=models.CharField(max_length=15),
+        ),
+    ]
+
